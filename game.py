4b252e359aaee33eacd96a39a5763f97787ef8fd
miras24412342 <123807395+miras24412342@users.noreply.github.com>
2023-01-28 17:28:15 +0600

    Add files via upload

---
+++ game.py
@@ -0,0 +1,15 @@
+# напиши здесь код основного окна игры
+
+from direct.showbase.ShowBase import ShowBase
+
+from mapmanager import Mapmanager
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.land = Mapmanager()
+        self.land.loadLand('land.txt')
+        base.camLens.setFov(90)
+
+game = Game()
+game.run()
